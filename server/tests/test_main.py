1b951322e64a2584b5d7a96799684ac3c9b37bfe
merytpeters <merytpeters@gmail.com>
2025-09-13 14:05:50 +0100

    feat: Add FastAPI app with health check and home route, and implement tests

---
+++ server/tests/test_main.py
@@ -0,0 +1,20 @@
+"""Test for app entry point"""
+from fastapi.testclient import TestClient
+from server.apps.main import app
+
+
+client = TestClient(app)
+
+
+def test_home():
+    """Test for welcome function"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello"}
+
+
+def test_health():
+    """Test for health check"""
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
